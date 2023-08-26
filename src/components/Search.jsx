f7a16ac85fcdeeebdc9d9e6f8ee47358f85b3bd3
frostyx-vk <vk.xam@yandex.ru>
2023-08-26 19:26:55 +0700

    add logic to search in fetch

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,6 +6,12 @@ class Search extends React.Component {
         search: ''
     }
 
+    handleKey = (e) => {
+        if(e.key === 'Enter'){
+            this.props.searchMovies(this.state.search);
+        }
+    }
+
     render() {
 
         return <div className="row">
@@ -15,7 +21,9 @@ class Search extends React.Component {
                     className="validate" 
                     placeholder="search"
                     value={this.state.search}
-                    onChange={(e) => this.setState({search: e.target.value})}/>
+                    onChange={(e) => this.setState({search: e.target.value})}
+                    onKeyDown={this.handleKey}/>
+                    <button className="btn search-btn" onClick={() => this.props.searchMovies(this.state.search)}>Search</button>
                 </div>
         </div>
 
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -15,11 +15,17 @@ class Main extends React.Component {
         .then(data => this.setState({movies: data.Search}))
     }
 
+    searchMovies = (str) => {
+        fetch(`http://www.omdbapi.com/?apikey=926935bd&s=${str}`)
+        .then(response => response.json())
+        .then(data => this.setState({movies: data.Search}))
+    }
+
     render() {
         const {movies} = this.state;
 
         return <main className="container content">
-            <Search />
+            <Search searchMovies={this.searchMovies} />
             {
                 movies.length ? <Movies movies={this.state.movies} /> : <Preloader />
             }
