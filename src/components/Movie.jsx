b7aaa0bc19998cdd7b342e76e0c8326cf07c8757
frostyx-vk <vk.xam@yandex.ru>
2023-08-27 11:04:25 +0700

    add checkboxes

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -12,7 +12,7 @@ function Movie(props) {
     return <div id={id} className="card movie">
         <div className="card-image waves-effect waves-block waves-light">
             {
-                poster === 'N/A' ? <img className="activator" src={'https://placehold.co/600x400'} /> : <img className="activator" src={poster} />
+                poster === 'N/A' ? <img className="activator" src={'https://placehold.co/300x400'} /> : <img className="activator" src={poster} width={300} height={400}/>
             }
         </div>
         <div className="card-content">
